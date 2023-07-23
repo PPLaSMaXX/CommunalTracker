a619131aeda8764d532834969ff2e23fc6131e82
PLaSMaX <maximfedorko@gmail.com>
2023-07-24 02:40:28 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommunalTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
